ce5f7c0dfc8d6865d9ff9141999d3c3b5cbcd1a5
Prabal Srivastava <prabalworld2020ster@gmail.com>
2020-09-06 00:49:37 +0530

    19dg2g: In this code commit, added the bootstrap project

---
+++ bootstrap/src/main/java/com/socgen/bookmark/BookmarkServiceApplication.java
@@ -0,0 +1,11 @@
+package com.socgen.bookmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkServiceApplication.class, args);
+	}
+}
