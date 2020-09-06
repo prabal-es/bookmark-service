7d83e1bc55d1d0719b64e2f5ad46231872b5740c
Prabal Srivastava <prabalworld2020ster@gmail.com>
2020-09-06 20:26:05 +0530

    19dg2g: In this code commit, added the Rest API jUnit configuration

---
+++ rest-adapter/src/test/java/com/socgen/bookmark/rest/HelloTest.java
@@ -0,0 +1,37 @@
+package com.socgen.bookmark.rest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(value = Hello.class)
+public class HelloTest {
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	public HelloTest(final MockMvc mockMvc) {
+		this.mockMvc = mockMvc;
+	}
+
+	@Test
+	@DisplayName("shoud give greatings from hello API")
+	public void shouldGiveGreatingsFromHelloApi() throws Exception {
+		mockMvc.perform(get("/api/v1/hello"))/*.andDo(print())*/.andExpect(status().isOk())
+				.andExpect(content().string("hello world!!"));
+	}
+	
+	@Test
+	@DisplayName("shoud give greatings to universe from hello API")
+	public void shouldGiveGreatingsToUniverseFromHelloApi() throws Exception {
+		mockMvc.perform(get("/api/v1/hello").queryParam("test", "universe"))
+				/* .andDo(print()) */.andExpect(status().isOk())
+				.andExpect(content().string("hello universe"));
+	}
+}
