1a711ec60327cff72da3c5c152d5936fc55c2a32
Prabal Srivastava <prabalworld2020ster@gmail.com>
2020-09-08 01:34:14 +0530

    In this code commit, added the initial project structure for domain and jpa-adapter

---
+++ domain-api/src/main/java/com/socgen/bookmark/domain/App.java
@@ -0,0 +1,13 @@
+package com.socgen.bookmark.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
