fffc7bce2d558cd54fdbe6aa7abefe8489cb1918
Prabal Srivastava <prabalworld2020ster@gmail.com>
2020-09-12 11:04:59 +0530

    19dg2d: In this code commit, added the support for CrossOrigin for UI

--- rest-adapter/src/main/java/com/socgen/bookmark/rest/CompanyController.java
+++ rest-adapter/src/main/java/com/socgen/bookmark/rest/CompanyController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/companies")
 @Tag(name = "Company")
 public class CompanyController {
